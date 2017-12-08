a96615bcffdb97e4d55a74dcc6d8da651661158e
V <s.vijay0991@yahoo.com>
2017-12-07 22:31:14 -0800

    adding first version

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: [
+        './source/App.js'
+    ],
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            query  :{
+                presets:['react','es2015']
+            }
+        }]
+    }
+};
